5ea393fd38524edbcc5357caec63feafe5588399
Felix Zemmel <fzemmel@users.noreply.github.com>
2026-07-29 09:04:30 +0200

    feat: add Vercel Web Analytics (#19)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Analytics } from "@vercel/analytics/react";
 import { TennisMixer } from "./components/TennisMixer/TennisMixer";
 
 export default function App() {
-  return <TennisMixer />;
+  return (
+    <>
+      <TennisMixer />
+      <Analytics debug={false} />
+    </>
+  );
 }
